a50af5a554415a48acd2ac068397538836e14e38
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-02 22:55:11 -0500

    8._Endpoints_de_Renover,_crear_y_login

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,16 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	res.json({
-		ok: true
-	});
-});
+const {
+	crearUsuario,
+	loginUsuario,
+	revalidarToken
+} = require('../controllers/auth');
+
+router.post('/new', crearUsuario);
+
+router.post('/', loginUsuario);
+
+router.get('/renew', revalidarToken);
 
 module.exports = router;
