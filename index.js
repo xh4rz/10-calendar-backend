2f53f96e5b4cab4119c95b2b5f885f28512fd55a
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-02 21:56:07 -0500

    5._Configurando_Express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hola Mundo');
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+	res.json({
+		ok: true
+	});
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+	console.log(`Servidor corriendo en puerto ${4000}`);
+});
