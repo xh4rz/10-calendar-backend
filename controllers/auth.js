e7327ae5f2790ac86013f33122e166d4e1cc2aa0
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-03 23:42:30 -0500

    10._Express_Validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,20 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 	const { name, email, password } = req.body;
 
-	if (name.length < 5) {
+	// manejo de errores
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
 		return res.status(400).json({
 			ok: false,
-			msg: 'El nombre debe de ser de 5 letras'
+			errors: errors.mapped()
 		});
 	}
 
-	res.json({
+	res.status(201).json({
 		ok: true,
 		msg: 'registro',
 		name,
@@ -22,7 +26,17 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
 	const { email, password } = req.body;
 
-	res.json({
+	// manejo de errores
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			ok: false,
+			errors: errors.mapped()
+		});
+	}
+
+	res.status(201).json({
 		ok: true,
 		msg: 'login',
 		email,
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@
 
 const { Router } = require('express');
 
+const { check } = require('express-validator');
+
 const router = Router();
 
 const {
@@ -13,9 +15,30 @@ const {
 	revalidarToken
 } = require('../controllers/auth');
 
-router.post('/new', crearUsuario);
+router.post(
+	'/new',
+	[
+		// middlewares
+		check('name', 'El nombre es obligatorio').not().isEmpty(),
+		check('email', 'El email es obligatorio').isEmail(),
+		check('password', 'El password debe ser de mínimo 6 caracteres').isLength({
+			min: 6
+		})
+	],
+	crearUsuario
+);
 
-router.post('/', loginUsuario);
+router.post(
+	'/',
+	[
+		// middlewares
+		check('email', 'El email es obligatorio').isEmail(),
+		check('password', 'El password debe ser de mínimo 6 caracteres').isLength({
+			min: 6
+		})
+	],
+	loginUsuario
+);
 
 router.get('/renew', revalidarToken);
 
