2c1e28e1398a5da07a5b8ade795d6360150d9531
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-07 21:49:35 -0500

    16._Encriptar_la_contrase+¦a

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
 const crearUsuario = async (req, res = response) => {
@@ -15,6 +16,10 @@ const crearUsuario = async (req, res = response) => {
 		}
 
 		usuario = new Usuario(req.body);
+
+		// Encriptar contraseña
+		const salt = bcrypt.genSaltSync();
+		usuario.password = bcrypt.hashSync(password, salt);
 
 		await usuario.save();
 
