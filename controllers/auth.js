25f56383eb8d5c4a8ab9743fd8e9a2fe95afeb36
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-07 21:23:39 -0500

    15._Validaciones_del_usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,26 @@ const { response } = require('express');
 const Usuario = require('../models/Usuario');
 
 const crearUsuario = async (req, res = response) => {
-	// const { name, email, password } = req.body;
+	const { email, password } = req.body;
 
 	try {
-		const usuario = new Usuario(req.body);
+		let usuario = await Usuario.findOne({ email });
+
+		if (usuario) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Un usuario existe con ese correo'
+			});
+		}
+
+		usuario = new Usuario(req.body);
 
 		await usuario.save();
 
 		res.status(201).json({
 			ok: true,
-			msg: 'registro'
+			uid: usuario.id,
+			name: usuario.name
 		});
 	} catch (error) {
 		console.log(error);
