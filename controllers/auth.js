cb327e9fd226b6f8960d9e0cca2ddd0c5af89cc3
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-07 21:14:14 -0500

    14._Crear_un_usuario_en_nuestra_Base_de_Datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,25 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
-	const { name, email, password } = req.body;
+const crearUsuario = async (req, res = response) => {
+	// const { name, email, password } = req.body;
 
-	res.status(201).json({
-		ok: true,
-		msg: 'registro',
-		name,
-		email,
-		password
-	});
+	try {
+		const usuario = new Usuario(req.body);
+
+		await usuario.save();
+
+		res.status(201).json({
+			ok: true,
+			msg: 'registro'
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Por favor hable con el administrador'
+		});
+	}
 };
 
 const loginUsuario = (req, res = response) => {
