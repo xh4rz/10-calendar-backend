c3ba3c6db3bc3aee5054bfc9be56f508420b36b3
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-07 23:32:12 -0500

    18._Generar_un_Json_Web_Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async (req, res = response) => {
 	const { email, password } = req.body;
@@ -23,10 +24,14 @@ const crearUsuario = async (req, res = response) => {
 
 		await usuario.save();
 
+		// Generar JWT
+		const token = await generarJWT(usuario.id, usuario.name);
+
 		res.status(201).json({
 			ok: true,
 			uid: usuario.id,
-			name: usuario.name
+			name: usuario.name,
+			token
 		});
 	} catch (error) {
 		console.log(error);
@@ -60,11 +65,14 @@ const loginUsuario = async (req, res = response) => {
 			});
 		}
 
-		// Generar nuestro JWT
+		// Generar JWT
+		const token = await generarJWT(usuario.id, usuario.name);
+
 		res.json({
 			ok: true,
 			uid: usuario.id,
-			name: usuario.name
+			name: usuario.name,
+			token
 		});
 	} catch (error) {
 		console.log(error);
