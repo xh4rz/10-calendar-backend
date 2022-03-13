3c40248639b374a5de152301f1b484f21f1deefc
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-12 21:37:39 -0500

    6._Validar_campos_necesarios

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,9 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const {
 	getEventos,
@@ -21,7 +24,16 @@ router.use(validarJWT);
 router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', crearEvento);
+router.post(
+	'/',
+	[
+		check('title', 'El titulo es obligatorio').not().isEmpty(),
+		check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+		check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+		validarCampos
+	],
+	crearEvento
+);
 
 // Actualizar Evento
 router.put('/:id', actualizarEvento);
