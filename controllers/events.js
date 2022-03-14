b1c5c3f81cf58606d6d167169d9c223cd162d526
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-13 21:38:04 -0500

    10._Eliminar_Eventos

--- controllers/events.js
+++ controllers/events.js
@@ -39,7 +39,7 @@ const actualizarEvento = async (req, res = response) => {
 		const evento = await Evento.findById(eventoId);
 
 		if (!evento) {
-			res.status(404).json({
+			return res.status(404).json({
 				ok: false,
 				msg: 'Evento no existe por ese id'
 			});
@@ -76,11 +76,39 @@ const actualizarEvento = async (req, res = response) => {
 	}
 };
 
-const eliminarEvento = (req, res = response) => {
-	res.json({
-		ok: true,
-		msg: 'eliminarEvento'
-	});
+const eliminarEvento = async (req, res = response) => {
+	const eventoId = req.params.id;
+	const uid = req.uid;
+
+	try {
+		const evento = await Evento.findById(eventoId);
+
+		if (!evento) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'Evento no existe por ese id'
+			});
+		}
+
+		if (evento.user.toString() !== uid) {
+			return res.status(401).json({
+				ok: false,
+				msg: 'No tiene privilegio de eliminar este evento'
+			});
+		}
+
+		await Evento.findByIdAndDelete(eventoId);
+
+		res.json({
+			ok: true
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Hable con el administrador'
+		});
+	}
 };
 
 module.exports = {
